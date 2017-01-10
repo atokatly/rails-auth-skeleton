8f7ff06a818a1ebb79d4aeee67ed6133582680ab
Anthony Tokatly <atokatly884@gmail.com>
2017-01-09 16:43:31 -0800

    add auth skeleton

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
---
+++ db/migrate/20170110002718_createusers.rb
@@ -0,0 +1,12 @@
+class Createusers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :username, :null => false
+      t.string :email, :null => false
+      t.string :password_digest
+
+
+      t.timestamps null: false
+    end
+  end
+end
